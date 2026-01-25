b874a60e4e0a5a22d0eebcb5c88d4a75cf1cb29b
Akbota <v.akbota.7@gmail.com>
2026-01-26 02:23:19 +0500

    data types of python

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,20 @@
+#1.strings
+x="hello world"
+print(x)
+#2.int
+x=123
+print(int(x))
+
+#3.list
+x=["red", "blue","orange"]
+print(x)
+
+#4.float
+x=12.7
+print(x) #or
+print(float(x))
+
+#5.boolean
+x=True
+if x:
+  print("hello")
