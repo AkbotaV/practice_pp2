8890fbc67b427cc841858e770813a95fc58a625a
Akbota <v.akbota.7@gmail.com>
2026-01-26 02:36:23 +0500

    Added types of numbers

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,44 @@
+#1.int
+x=34411233424455
+y=-123123
+z=0
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2.float
+x=234423.433
+y=-3.33
+z=0.0
+print(type(x))
+print(type(y))
+print(type(z))
+
+#3.floats scientific
+x=1e3
+y=12E4
+z=-12.E23
+print(type(x))
+print(type(y))
+print(type(z))
+
+#4.Complex numbers
+x=3+5j
+y=4j
+z=-23j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#5.converting to another type
+x=12
+y=1.2
+z=3j
+
+a=int(y)
+b=float(x)
+c=complex(x)
+
+print(type(a))
+print(type(b))
+print(type(c))
