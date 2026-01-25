2a96f9f8240885119902ae90ef027eb2dbc4a34b
Akbota <v.akbota.7@gmail.com>
2026-01-26 01:47:21 +0500

    Add types of comments

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,21 @@
+#1.This is a comment
+print("hello world")
+
+print("hi") #2. This is a comment
+
+#3.
+#print("Hello, World!")
+print("Cheers!")
+
+#4.
+#this is comment
+#written in multip 
+#lines
+#not multiline comment
+print("hello world")
+
+#5.
+"""this can also be comment,
+but its string."""
+print("hello wrld")
+
