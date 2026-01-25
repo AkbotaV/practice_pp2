84551495ccaded455d25fc835fa6f93573480714
Akbota <v.akbota.7@gmail.com>
2026-01-26 02:13:30 +0500

    add variables topic

---
+++ Practice1/python-basics/variables.py
@@ -0,0 +1,28 @@
+#1.
+x =127
+y="Akbota"
+print(x)
+print(y)
+
+#2. variable can change,print the new one
+x=123
+x="one"
+print(x)
+
+#3.you can specify data type of var
+x=int(3) # 3
+y=str(3) # '3'
+z=float(3) # 3.0
+print(x,y,z)
+
+#4
+x=1234
+print(type(x))
+y="abcde"
+print(type(y))
+
+#5.multiple variables
+x, y, z=1,2,3
+print(x)
+print(y)
+print(z)
