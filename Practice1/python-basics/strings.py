b0f43ab8786e0378632c4c8977645fb1efa1ffed
Akbota <v.akbota.7@gmail.com>
2026-01-26 02:50:07 +0500

    strings in python added

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,26 @@
+#1.same
+print('hello')
+print("hello")
+
+#2.quotes inside of strings
+print('he said "ok" ')
+print("she said 'hi'")
+
+#3.
+a="hiii"
+print(a)
+
+#4.Multiline Strings
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#5.str is array
+a = "Hello, World!"
+print(a[1])
+
+#6.length
+a = "Hello, World!"
+print(len(a))
