32f2497cd71b763f54035463fbe691498d041650
Akbota <v.akbota.7@gmail.com>
2026-02-02 00:11:29 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice_2/boolean/boolean_comparison.py
@@ -0,0 +1,32 @@
+#1Comparing numbers
+a = 10
+b = 5
+print(a > b)      #True
+
+#2Equality check
+x=7
+y=7
+print(x == y)     #True
+
+#3Not equal
+p = 3
+q = 9
+print(p != q)     #True
+
+#4Comparison using a variable
+age = 16
+is_adult = age >= 18
+print(is_adult)   #False
+
+#5 String comparison
+name1 = "Ayaulym"
+name2 = "Aruzhan"
+print(name1 == name2)   # False
+
+#6 Greater than or equal to
+score = 75
+print(score >= 50)      # True
+
+#7 Less than or equal to
+temperature = -5
+print(temperature <= 0)  # True
